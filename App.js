2b9ac4b57d2b5da64f6b261a3b069222bdf34741
SulavThapa <46512631+SulavThapa@users.noreply.github.com>
2020-06-01 19:14:09 +0545

    First Commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import{
+  SafeAreaView,
+  Text
+} from 'react-native';
+
+class App extends React.Component{
+  render(){
+    return(
+      <SafeAreaView>
+        <Text style={{textAlign: 'center'}}>
+          Get Started
+        </Text>
+      </SafeAreaView>
+    );
+  }
+}
+
+export default App;
